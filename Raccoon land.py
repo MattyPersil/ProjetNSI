45624917b1759a37167aa6acb52c35d30c683567
Matthieu <matthieumm444444@gmail.com>
2024-02-02 10:31:48 +0100

    Update Raccoon land.py

--- Raccoon land.py
+++ Raccoon land.py
@@ -96,7 +96,7 @@ class  blcks(pygame.sprite.Sprite):
         self.specialrect = None
         self.gravite = 10
         self.resistance = 0  
-        self.levels = [(level_test_1,level_test_2),(level_2_world_1,level_2_world_2)]
+        self.levels = [(level_test_1,level_test_2),(level_2_world_1,level_2_world_2),(level_3_world_1,level_3_world_2)]
         self.wld = 0
     
     def display(self):
