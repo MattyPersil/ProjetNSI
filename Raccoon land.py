77705ba50b77cd9efe5aeb19915ca6e5bd8d6a5f
LucasMolendi <lucasmolendi0@gmail.com>
2024-02-16 10:35:29 +0100

    dsds

--- Raccoon land.py
+++ Raccoon land.py
@@ -92,8 +92,13 @@ class  blcks(pygame.sprite.Sprite):
         self.flowers = pygame.transform.scale(pygame.image.load("assets/flowers.png"),self.image_size)
         self.cloud = pygame.transform.scale(pygame.image.load("assets/cloud.png"),self.image_size)
         self.change_level_block = pygame.transform.scale(pygame.image.load("assets/Change Level.png"),self.image_size)
-        self.spike = pygame.transform.scale(pygame.image.load("assets/pique.png"),self.image_size)
-        self.spike = pygame.transform.scale(pygame.image.load("assets/hellstone.png"),self.image_size)
+        self.spike = pygame.transform.scale(pygame.image.load("assets/spike.png"),self.image_size)
+        self.hellstone = pygame.transform.scale(pygame.image.load("assets/hellstone.png"),self.image_size)
+        self.helldirt = pygame.transform.scale(pygame.image.load("assets/helldirt.png"),self.image_size)
+        self.hellgrass = pygame.transform.scale(pygame.image.load("assets/hellgrass.png"),self.image_size)
+        self.helltrashcan = pygame.transform.scale(pygame.image.load("assets/helltrashcan.png"),self.image_size)
+        self.hellflower = pygame.transform.scale(pygame.image.load("assets/hellflowers.png"),self.image_size)
+        self.hellspike = pygame.transform.scale(pygame.image.load("assets/hellspike.png"),self.image_size)
         self.rects = [] 
         self.specialrect = None
         self.gravite = 10
