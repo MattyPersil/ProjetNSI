d66b721d96dbf291a8fda91accb1cf9ef1e9400d
Matthieu <matthieumm444444@gmail.com>
2024-03-15 10:52:18 +0100

    f

--- Raccoon land.py
+++ Raccoon land.py
@@ -162,7 +162,7 @@ class Minigame:
         self.minigame_golden_trash_image = pygame.transform.scale(pygame.image.load("assets/golden_trash.png"),((screen.get_width()/27)+1,(screen.get_height()/16)+1))
         self.minigame_deadly_trash_image = pygame.transform.scale(pygame.image.load("assets/deadly_trash.png"),((screen.get_width()/27)+1,(screen.get_height()/16)+1))
         self.minigame_background = pygame.transform.scale(pygame.image.load("assets/ground.png"),(1000,600))
-        self.minigame_levels = [level_1_mini]
+        self.minigame_levels = [level_1_mini,level_2_mini,level_3_mini,level_4_mini]
     
     #fonction render permettant d'afficher le minijeu
     def render(self,image_size,actual_level):
