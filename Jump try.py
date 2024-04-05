30f344f1681255bdb1b1bda47a2ae79ed8c7f5b5
Matthieu <matthieumm444444@gmail.com>
2024-04-05 10:38:39 +0200

    jump

--- Jump try.py
+++ Jump try.py
@@ -13,13 +13,15 @@ x = screen.get_width()/27
 image = pygame.transform.scale(pygame.image.load("assets/pizza.png"), (x,y))
 ground =pygame.transform.scale(pygame.image.load("assets/ground.jpg"), (1000,600))
 bg = pygame.transform.scale(pygame.image.load("assets/sky0.png"), (1000,600))
+bloc = pygame.transform.scale(pygame.image.load("assets/stone.png"),(100,100))
+
 image_coords = [0,0]
 image_rect = image.get_rect(topleft = image_coords)
 
 #is_jumping = False
 is_falling = True
 ground_rect = ground.get_rect(topleft = (0,y*15))
-
+bloc_rect = bloc.get_rect(topleft = (x*20,y*9))
 movey = 0
 collision_cd = 0
 
@@ -27,26 +29,31 @@ def collisions():
     global ground_rect,image_rect,collision_cd
     if collision_cd >0:
         collision_cd-=1
+
     else:
-        if ground_rect.colliderect(image_rect):
-            return True
-        else:
-            return False
+        value=False
+        for object in [ground_rect,bloc_rect]:
+            if object.colliderect(image_rect):
+                value = True
+            elif value != True:
+                value = False
+        return value
 
 def stick():
-    pass
-    """global ground_rect,image_rect
+    global ground_rect,image_rect
     while collisions()==True:
-        image_rect.move(image_rect.x,image_rect.y-5)"""
+        image_rect = image_rect.move(image_rect.x,image_rect.y-10)
 
 def gravity():
-    global image_rect, is_falling, movey
-    if is_falling == True:
+    global is_falling, movey
+    if is_falling == True or collisions() == False:
         movey += 1
     if collisions() == True:
         movey = 0
         stick()
         is_falling = False
+    
+
 
 def jump():
     global movey,is_falling,collision_cd
@@ -65,7 +72,7 @@ while running == True:
     screen.blit(bg, (0,0))
     screen.blit(ground,ground_rect)
     screen.blit(image,image_rect)
-
+    screen.blit(bloc,bloc_rect)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
